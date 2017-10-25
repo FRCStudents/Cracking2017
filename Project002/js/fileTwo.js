9189de1b536c7dd074edcaeb39b89bd5fbf64387
Efraim Krug <ekrug@foxboroughrcs.org>
2017-10-24 21:09:03 -0400

    Adding project002

---
+++ Project002/js/fileTwo.js
@@ -0,0 +1,82 @@
+
+function init(){
+  var fname = document.getElementById("fname");
+  var lname = document.getElementById("lname");
+  var answer = document.getElementById("cell6");
+
+  fname.innerHTML = "";
+  lname.innerHTML = "";
+  answer.innerHTML = "";
+
+}
+function scootch(){
+  alert("Scootch");
+}
+/****
+ * @param sep - one character separator
+ * This function responds to one of 5 buttons.
+ * Each button passes a 1-char value to use as the separator
+ ****/
+function doit(sep){
+  var fname = document.getElementById("fname");
+  var lname = document.getElementById("lname");
+  var answer = document.getElementById("cell6");
+
+  var fullName = sep + fname.value + sep + lname.value + sep;
+  answer.innerHTML = fullName;
+}
+
+function doit2(){
+  var fname = document.getElementById("fname");
+  var lname = document.getElementById("lname");
+  var answer = document.getElementById("cell6");
+  var sep = document.getElementById("sep");
+
+  /****
+   * needs some validation
+   ****/
+  var pattern = /[\& | \# | \@ | \~ | \*]/;
+  var fullName = "";
+
+  if(pattern.test(sep.value)){
+    fullName = sep.value + fname.value + sep.value + lname.value + sep.value;
+  } else {
+    fullName = fname.value + " " + lname.value;
+    alert("Sorry, there was a serious problem with the separator you chose!");
+  }
+  answer.innerHTML = fullName;
+}
+
+function doit3(){
+  var fname = document.getElementById("fname");
+  var lname = document.getElementById("lname");
+  var answer = document.getElementById("cell6");
+
+  var sepStar = document.getElementById("r*");
+  var sepAmp = document.getElementById("r&");
+  var sepAt = document.getElementById("r@");
+  var sepPound = document.getElementById("r#");
+  var sepTilda = document.getElementById("r~");
+
+  var sep = "";
+
+  if(sepStar.checked){
+    sep = "*";
+  }
+  if(sepAmp.checked){
+    sep = "&";
+  }
+  if(sepAt.checked){
+    sep = "@";
+  }
+  if(sepPound.checked){
+    sep = "#";
+  }
+  if(sepTilda.checked){
+    sep = "~";
+  }
+
+  fullName = sep + fname.value + sep + lname.value + sep;
+  answer.innerHTML = fullName;
+
+}

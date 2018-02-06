4cd93e41f873ae05465f59a423dfc260d056b180
redjayphoenix <redjayphoenix@24dcaea7d349>
2018-02-06 16:10:55 +0000

    hi

---
+++ JaredGEtienne/coding/FEB3.py
@@ -0,0 +1,9 @@
+arr= [1,2,3,4,5,6,7,8,9,10]
+print("This is the array")
+for x in range (0,10):
+    print(arr[x])
+print("this is the array reversed")
+for x in range (9, -1, -1):
+    print(arr[x])
+
+
---
+++ JaredGEtienne/coding/FEB4.py
@@ -0,0 +1,6 @@
+arr= [1,2,1,2,1,2,1,2,1,2]
+holder=0
+for x in range (0,10):
+    if  arr[x] = 1 :
+        holder + 1
+print(holder)

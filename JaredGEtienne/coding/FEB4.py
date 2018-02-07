4380d2eba622daa9d8d56c7dc3ab894c81927ed3
redjayphoenix <redjayphoenix@24dcaea7d349>
2018-02-07 14:15:04 +0000

    hi

--- JaredGEtienne/coding/FEB4.py
+++ JaredGEtienne/coding/FEB4.py
@@ -1,6 +1,8 @@
 arr= [1,2,1,2,1,2,1,2,1,2]
 holder=0
+a = 1
+b=0
 for x in range (0,10):
-    if  arr[x] = 1 :
-        holder + 1
-print(holder)
+    if a == arr[x] :
+       b += holder + 1
+print(b)

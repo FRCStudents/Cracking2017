4532728a4a21e81fb48d60b7702ec71d612d1e0c
redjayphoenix <redjayphoenix@24dcaea7d349>
2018-02-11 23:08:52 +0000

    hi

--- JaredGEtienne/coding/FEB1.py
+++ JaredGEtienne/coding/FEB1.py
@@ -1,13 +1,6 @@
-arr = [1,2,3,4,5,6,7,8,9,10]
-print("this is what the array is at part 3")
-print( arr[3])
-print("this is the length of the array")
-print (len(arr))
-print("this is the array itself")
-print (arr)
-print( "this is the array printed one at a time")
-for x in range (0,10):
-    print(arr[x])
-
-
-
+def yoyoyo():
+    arr = [1,2,3,4,5]
+    print(arr)
+    print("this is arr at the second value")
+    print(arr[1])
+yoyoyo()

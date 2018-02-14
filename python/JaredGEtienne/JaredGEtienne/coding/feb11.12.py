c510489bfe38d06c845a17e534bd202593bc4329
Efraim Krug <ekrug@foxboroughrcs.org>
2018-02-14 11:25:03 -0500

    readme notes

--- python/JaredGEtienne/JaredGEtienne/coding/feb11.12.py
+++ python/JaredGEtienne/JaredGEtienne/coding/feb11.12.py
@@ -1,9 +1,8 @@
-a=[1,1,1,1,1]:
-  2 
-    3 print("This is a " a)
-      4 def plusOne():
-            5     for x in range(len(a)):
-                  6         a[x] = a[x] + 1
-                    7     print("this is a after we increment each value " a)
-                      8 plusOne()
-                        9 plusOne() 
+a=[1,1,1,1,1]
+print("This is a " a)
+def plusOne():
+   for x in range(len(a)):
+       a[x] = a[x] + 1
+       print("this is a after we increment each value " a)
+plusOne()
+plusOne() 

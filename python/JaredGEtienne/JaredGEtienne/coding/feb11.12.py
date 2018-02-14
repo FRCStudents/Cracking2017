e90424946324fd4862f131411fe374bbb2ab8d8c
Efraim Krug <ekrug@foxboroughrcs.org>
2018-02-14 10:47:58 -0500

    changing directories around a bit

---
+++ python/JaredGEtienne/JaredGEtienne/coding/feb11.12.py
@@ -0,0 +1,9 @@
+a=[1,1,1,1,1]:
+  2 
+    3 print("This is a " a)
+      4 def plusOne():
+            5     for x in range(len(a)):
+                  6         a[x] = a[x] + 1
+                    7     print("this is a after we increment each value " a)
+                      8 plusOne()
+                        9 plusOne() 

fd63b04d548d4460cde117ef5bfef2f733f50ab2
redjayphoenix <redjayphoenix@24dcaea7d349>
2018-02-15 20:18:10 +0000

    the somewhat working version

--- python/JaredGEtienne/JaredGEtienne/coding/CaesarsSquare.py
+++ python/JaredGEtienne/JaredGEtienne/coding/CaesarsSquare.py
@@ -7,27 +7,21 @@ array2 = "abcdefghijklmnopqrstuvwxyz"
 #x = Brutus.find("y")
 #print(x)
 b = []
-print(len(array1[0:5]))
 for x in range(len(Brutus)):
     for r in range(0,25):
         if Brutus[x] == array2[r]:
-            print(len(array2[0:r+1]))
             a = len(array2[0:r+1])
-            print(a)
             b.append(len(array2[0:r+1]) + int(plus))
-            print(b)
-            print("that was b")
     if Brutus[x] == array2[25]:
         b.append(int(plus) - 1)
-        if len(b)== len(Brutus):
-            print(b)
+print("this is the value of the array increased")
+print(b)
 print("this is the conversion")
 print(b)
 print(Brutus)
-print("this is array 2")
+print("this is array increased")
 for y in range(len(b)):
     for z in range(0,25):
         if b[y] == len(array2[0:z+1]):
-            print(b)
             b[y] = array2[z]
-            print(b)
+print(b)

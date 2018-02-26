29b72723c28ee17cf244c31dcf72db35f2302ffb
Nico <kingnajac628@gmail.com>
2018-02-26 15:35:57 +0000

    Adding race

---
+++ python/remix.py
@@ -0,0 +1,10 @@
+remix = ['b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','r','q','s','t','u','v','w','x','y','z','a']
+toy = print ("upz")
+apple = print("bqqmf")
+run = print("svo")
+sport = print("tqpsu")
+good = print("hppe")
+luck = print(mvdl)
+string = print(tusjoh)
+transposes = print(usbotqptft)
+

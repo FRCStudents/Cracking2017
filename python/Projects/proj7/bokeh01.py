82b9b6a9a5d875faed31ac6e0bc9ab4d4f99be66
hu10julie <hu10julie@gmail.com>
2018-02-27 12:48:38 -0500

    Fix things

---
+++ python/Projects/proj7/bokeh01.py
@@ -0,0 +1,21 @@
+# need to import at least 3 things to make your
+# bokeh plots work
+from bokeh.plotting import figure, show, output_file
+#
+# we specify an HTML file where the output will go
+# if we don't - output goes to bokeh01.html (filename.html)
+#
+output_file("plot.html")
+
+# load our x and y data
+x = [10, 20, 30]
+y = [4, 5, 6]
+
+# create a figure
+p = figure()
+
+# create a histogram
+p.vbar(x=x, top=y, width=0.5)
+
+# render (show) the plot
+show(p)
